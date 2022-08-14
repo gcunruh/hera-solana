acb80983092382830e6bfa8c9ad9d635f0db407f
Gabriel Unruh <gabriel@valetechnology.co>
2022-08-14 13:02:47 -0600

    initialize fund instruction

--- programs/hera-solana/src/lib.rs
+++ programs/hera-solana/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_lang::prelude::Pubkey;
+use anchor_spl::{token::{Token, Mint, TokenAccount}};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +8,47 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod hera_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_fund(ctx: Context<InitializeFund>, idx: u64, fy_premium: f32, fy_allowable: f32, year: u16) -> Result<()> {
+        let fund_data = &mut ctx.accounts.fund_data;
+        fund_data.fy_premium = fy_premium;
+        fund_data.fy_allowable = fy_allowable;
+        fund_data.year = year;
         Ok(())
     }
 }
 
+#[account]
+pub struct FundData {
+    creator: Pubkey,
+    fy_premium: f32,
+    fy_allowable: f32,
+    year: u16
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(idx: u64)]
+pub struct InitializeFund<'info> {
+    #[account(
+        init,
+        seeds=[b"fund_data".as_ref(), idx.to_le_bytes().as_ref()],
+        bump,
+        payer = creator, 
+        space = 8 + 32 + 32 + 32 + 64
+    )]
+    fund_data: Account<'info, FundData>,
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    #[account(
+        init,
+        seeds=[b"fund".as_ref(), idx.to_le_bytes().as_ref()],
+        bump,
+        payer = creator,
+        token::mint = mint,
+        token::authority = fund_data,
+    )]
+    pub fund: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
